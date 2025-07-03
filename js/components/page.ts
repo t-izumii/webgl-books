e50df6d55ba65cd1400b2dedef23f8a51017fb79
t-izumii <120708670+t-izumii@users.noreply.github.com>
2025-07-03 10:35:40 +0900

    feature: boneの追加

--- js/components/page.ts
+++ js/components/page.ts
@@ -23,10 +23,43 @@ class Page {
       PAGE_SEGMENTS,
       2
     );
-    const pageMaterial: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({
+const pageMaterial: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({
       color: 0x00ff00,
+      // todo:error
+      skinning: true
     });
-    const page: THREE.Mesh = new THREE.Mesh(pageGeometry, pageMaterial);
+    const position = pageGeometry.attributes.position;
+    const vertex: THREE.Vector3 = new THREE.Vector3();
+    const skinIndices: any = [];
+    const skinWeights: any = [];
+    for ( let i = 0; i < position.count; i ++ ) {
+
+      vertex.fromBufferAttribute( position, i );
+
+      // compute skinIndex and skinWeight based on some configuration data
+      const y = ( vertex.y + PAGE_HEIGHT / 2 );
+      const skinIndex = Math.floor( y / PAGE_SEGMENT_HEIGHT );
+      const skinWeight = ( y % PAGE_SEGMENT_HEIGHT ) / PAGE_SEGMENT_HEIGHT;
+      skinIndices.push( skinIndex, skinIndex + 1, 0, 0 );
+      skinWeights.push( 1 - skinWeight, skinWeight, 0, 0 );
+    }
+
+    pageGeometry.setAttribute( 'skinIndex', new THREE.Uint16BufferAttribute( skinIndices, 4 ) );
+    pageGeometry.setAttribute( 'skinWeight', new THREE.Float32BufferAttribute( skinWeights, 4 ) );
+
+    const page: THREE.SkinnedMesh = new THREE.SkinnedMesh(pageGeometry, pageMaterial);
+
+    // todo:bone bug
+    const bones: THREE.Bone[] = [];
+    const numBones = PAGE_SEGMENTS + 1;
+    const skeleton = new THREE.Skeleton( bones );
+    const rootBone = skeleton.bones[ 0 ];
+
+    page.add( rootBone );
+    page.bind( skeleton );
+
+    skeleton.bones[ 0 ].rotation.x = -0.1;
+    skeleton.bones[ 1 ].rotation.x = 0.2;
     return page;
   }
 }
